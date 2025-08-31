9a0331657fa99982704e67bfddf23b8e516758bc
arun-357 <arunachalamnachiappan20@gmail.com>
2025-08-31 12:25:26 +0530

    update:postOrder

--- src/solutionUpdate/sol.py
+++ src/solutionUpdate/sol.py
@@ -1,22 +1,31 @@
-
-from typing import List
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 
 class Solution:
-    def maxProfit(self, k: int, prices: List[int]) -> int:
-        max_txn = k
-        n = len(prices)
-        # dp[t][d] = max profit with at most t transactions by day d
-        # t ∈ [0..2] (number of transactions)
-        # d ∈ [0..n-1] (days)
-        dp = [[0] * n for _ in range(max_txn + 1)]
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Flattens the binary tree to a linked list in-place following preorder traversal.
+        """
+        curr = root
 
-        for t in range(1, max_txn+1):
-            max_profit_so_far = -prices[0]
-            for d in range(1, n):
-                # Option 1: don't sell today, carry forward previous max
-                # Option 2: sell today, and find best buy day using max_so_far
-                dp[t][d] = max(dp[t][d-1], prices[d] + max_profit_so_far)
-                # Update max_so_far for future days
-                max_profit_so_far = max(max_profit_so_far, dp[t-1][d] - prices[d])
-        # last day upto max_txn 
-        return dp[max_txn][n-1]
+        # Traverse through the tree using the right pointer
+        while curr:
+            if curr.left:
+                # Find the rightmost node of the left subtree
+                p = curr.left
+                while p.right:
+                    p = p.right
+                
+                # Attach the original right subtree to this rightmost node
+                p.right = curr.right
+
+                # Move the left subtree to the right
+                curr.right = curr.left
+                curr.left = None   # Clear the left pointer
+            
+            # Move to the next right node (linked list style)
+            curr = curr.right
